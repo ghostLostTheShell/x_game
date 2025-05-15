794a5d89a22839bd8d5f2ee9ef1083a0002f8647
cc <2773653457@qq.com>
2025-05-15 19:23:24 +0800

    【g】

---
+++ Music/music_controller.gd
@@ -0,0 +1,19 @@
+extends Node
+
+@onready var music_player = $AudioStreamPlayer
+
+func _ready() -> void:
+	play_music(preload("res://Music/bg.mp3"))
+
+
+func play_music(track: AudioStream):
+	music_player.stream = track
+	music_player.play()
+
+
+func stop_music():
+	music_player.stop()
+
+
+func _on_audio_stream_player_finished() -> void:
+	music_player.play() 
