34fb173035fbf4cc8e3a93e4155906085605c31a
cc <2773653457@qq.com>
2025-05-18 00:53:29 +0800

    添加语音控制

--- main/setup_page/setup_page.gd
+++ main/setup_page/setup_page.gd
@@ -4,7 +4,7 @@ extends Control
 @onready var t_anti_aliasing_status_unenable = preload("res://main/setup_page/texture/按钮未激活.png")
 @onready var t_anti_aliasing_status_enable = preload("res://main/setup_page/texture/按钮激活.png")
 @onready var voice_status_ui = $TextureRect2/VoiceStatus
-
+@onready var  volumeProgressHSlider = $TextureRect2/Volume/Progress/HSlider
 # Called when the node enters the scene tree for the first time.
 var voice_status = false
 
@@ -14,6 +14,14 @@ func _ready() -> void:
 		voice_status_ui.texture_normal = t_anti_aliasing_status_enable
 	else:
 		voice_status_ui.texture_normal = t_anti_aliasing_status_unenable
+		
+	var bus_index = AudioServer.get_bus_index("Master")  # 获取主音频总线
+	var volume_db = AudioServer.get_bus_volume_db(bus_index)  # 获取音量（dB）
+	#print("当前音量（dB）:", volume_db)
+	#var linear_volume = db_to_linear(volume_db)
+	volumeProgressHSlider.value = db_to_linear(volume_db)
+	
+	#print("当前音量（线性值）:", linear_volume, "xx", linear_to_db(0.5))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -62,3 +70,8 @@ func _on_voice_status_pressed() -> void:
 
 func _on_brack_buttion_pressed() -> void:
 	visible = false
+
+
+func _on_h_slider_value_changed(value: float) -> void:
+	var bus_index = AudioServer.get_bus_index("Master")
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))  # 设置音量
